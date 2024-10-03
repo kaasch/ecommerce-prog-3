6e78025317bd499669d539ae41a47ef258ec9c16
kaasch <karensch04@gmail.com>
2024-10-03 19:42:40 -0300

    finalv2

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { useAppContext } from '@/app/contexts/AppContext';
 import Hero from '@/app/components/Hero/Hero';
 import FirstSection from '@/app/components/FirstSection/FirstSection';
-// import SectionCard from '@/app/components/SectionCard/SectionCard';
 
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
   return (
     <div>
       <Hero mainText={'Probá el sabor más rico en un bocado'} secondaryText={'Nuestras galletas están hechas con los mejores ingredientes que se siente en cada mordida. Perfectas para darte un gustito o para compartir con quienes más querés!'} />
-      {/* <SectionCard  key={index} item={item} /> */}
       <FirstSection sectionThreeText={'Obtené un 10% de descuento en tu primera compra!'}/>
     </div>
   );
