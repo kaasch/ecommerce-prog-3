7ea30c181dafbbd8fb5faf40aa12ad0fd908482a
Lean Amaro <leanamaro@gmail.com>
2024-11-13 19:42:14 -0300

    fixed buttonGrid

--- src/app/components/ButtonsGrid/ButtonsGrid.js
+++ src/app/components/ButtonsGrid/ButtonsGrid.js
@@ -1,41 +1,63 @@
+'use client';
 
 import ButtonSingle from '@/app/components/ButtonSingle/ButtonSingle';
-import { getAllChipsDB, getAllMasaDB, getAllToppingDB } from '@/app/actions';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-export default async function ButtonsGrid() {
-    const  masaResponse = await getAllMasaDB()
-    const  chipsResponse = await getAllChipsDB()
-    const  toppingResponse = await getAllToppingDB()
+export default function ButtonsGrid() {
+  const [masas, setMasas] = useState([]);
+  const [chips, setChips] = useState([]);
+  const [toppings, setToppings] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  console.log("masa",await getAllMasaDB(), "chips",chipsResponse, "topping",toppingResponse)
+  const getData = async () => {
+    setLoading(true)
+    try {
+      const masaResponse = await axios.get('/api/masa');
+      const chipsResponse = await axios.get('/api/chips');
+      const toppingResponse = await axios.get('/api/topping');
+      setMasas(masaResponse.data.masa);
+      setChips(chipsResponse.data.chips);
+      setToppings(toppingResponse.data.topping);
+      setLoading(false)
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <section>
-      <div className="mb-12">
-        <h3 className="text-primary-color px-8 py-6 font-medium">Chips</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-3">
-          {chipsResponse.chips && chipsResponse.chips.map((item) => (
-            <ButtonSingle key={item._id} item={item} type="chips" />     
-          ))}
+      <div className='mb-12'>
+        <h3 className='text-primary-color px-8 py-6 font-medium'>Chips</h3>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-3'>
+          {!loading &&
+            chips.map((item) => (
+              <ButtonSingle key={item._id} item={item} type='chips' />
+            ))}
         </div>
       </div>
 
-       <div className="mb-12">
-        <h3 className="text-primary-color px-8 py-6 font-medium">Masa</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-3">
-          {masaResponse.masa && masaResponse.masa.map((item) => (
-            <ButtonSingle key={item._id} item={item} type="masa"/>     
-          ))}
+      <div className='mb-12'>
+        <h3 className='text-primary-color px-8 py-6 font-medium'>Masa</h3>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-3'>
+          {!loading &&
+            masas.map((item) => (
+              <ButtonSingle key={item._id} item={item} type='masa' />
+            ))}
         </div>
       </div>
 
-      <div className="mb-12">
-        <h3 className="text-primary-color px-8 py-6 font-medium">Toppings</h3>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-3">
-          {toppingResponse && toppingResponse.topping.map((item) => (
-            <ButtonSingle key={item._id} item={item} type="topping"/>     
-          ))}
+      <div className='mb-12'>
+        <h3 className='text-primary-color px-8 py-6 font-medium'>Toppings</h3>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 gap-3'>
+          {!loading &&
+            toppings.map((item) => (
+              <ButtonSingle key={item._id} item={item} type='topping' />
+            ))}
         </div>
-      </div> 
+      </div>
     </section>
   );
 }
